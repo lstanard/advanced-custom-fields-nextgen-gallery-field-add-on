09665712188fc2b32b8bfd8e22bbe94df88b415a
Charlie Stanard <louis.stanard@gmail.com>
2017-07-04 16:22:19 -0600

    Adding option for no gallery

--- acf-ngg-v5.php
+++ acf-ngg-v5.php
@@ -1,8 +1,8 @@
 <?php
 
 class acf_field_ngg extends acf_field {
-	
-	
+
+
 	/*
 	*  __construct
 	*
@@ -15,14 +15,14 @@ class acf_field_ngg extends acf_field {
 	*  @param	n/a
 	*  @return	n/a
 	*/
-	
+
 	function __construct() {
-		
+
 		// vars
 		$this->name = 'ngg';
 		$this->label = __('NextGEN Gallery', 'acf-ngg');
 		$this->category = 'relational';
-		
+
 		$this->defaults = array(
 			'input_type'		=> 'select',
 			'allow_null'		=> 0,
@@ -36,20 +36,20 @@ class acf_field_ngg extends acf_field {
 			'disabled'			=> 0,
 			'readonly'			=> 0,
 		);
-		
+
 		// ajax
 		//add_action('wp_ajax_acf/fields/select/query',				array($this, 'ajax_query'));
 		//add_action('wp_ajax_nopriv_acf/fields/select/query',		array($this, 'ajax_query'));
-		
+
 		// action
 		add_action( 'admin_enqueue_scripts', array($this, 'admin_enqueue_scripts') );
-		
+
 		// do not delete!
     	parent::__construct();
-		
+
 	}
-	
-	
+
+
 	/*
 	*  query_posts
 	*
@@ -62,16 +62,16 @@ class acf_field_ngg extends acf_field {
 	*  @param	n/a
 	*  @return	n/a
 	*/
-	
+
 	/*
-	
+
 	function ajax_query() {
-		
-			
+
+
 	}
-	
+
 	*/
-	
+
 	/*
 	*  render_field_settings()
 	*
@@ -84,7 +84,7 @@ class acf_field_ngg extends acf_field {
 	*  @param	$field (array) the $field being edited
 	*  @return	n/a
 	*/
-	
+
 	function render_field_settings( $field ) {
 
 		acf_render_field_setting( $field, array(
@@ -96,9 +96,9 @@ class acf_field_ngg extends acf_field {
 			'choices'		=> array(
 						'1' => __( 'Yes' ),
 						'0' => __( 'No' )
-                  )	
+                  )
 		));
-		
+
 		acf_render_field_setting( $field, array(
 			'label'			=> __('Nextgen Type'),
 			'instructions'	=> 'Allow or restrict the Nextgen selection type.',
@@ -109,9 +109,9 @@ class acf_field_ngg extends acf_field {
                         'Galleries and Albums'	=> __( 'Galleries and Albums' ),
                         'Galleries'				=> __( 'Galleries' ),
                         'Albums'				=> __( 'Albums' )
-                  )	
+                  )
 		));
-		
+
 		// multiple
 		acf_render_field_setting( $field, array(
 			'label'			=> __('Select multiple values?', 'acf'),
@@ -126,9 +126,9 @@ class acf_field_ngg extends acf_field {
                         'multiselect'	=> __( 'Multi-Select' )
                   )
 		));
-		
+
 		//if ($field['multiple'] == 'multiple') :
-		
+
 			// multiple_size
 			acf_render_field_setting( $field, array(
 				'label'			=> __('Multi-Select Size'),
@@ -140,9 +140,9 @@ class acf_field_ngg extends acf_field {
 				'layout'		=> 'horizontal',
 				'choices'		=> array_combine( range( 3, 15, 2 ), range( 3, 15, 2 ) )
 			));
-			
+
 		//endif;
-		
+
 		// ui
 		/*acf_render_field_setting( $field, array(
 			'label'			=> __('Stylised UI', 'acf'),
@@ -158,7 +158,7 @@ class acf_field_ngg extends acf_field {
 
 
 	}
-	
+
 	/*
 	*  ngg_get_galleries()
 	*
@@ -171,27 +171,27 @@ class acf_field_ngg extends acf_field {
 	*  @param	n/a
 	*  @return	array of galleries
 	*/
-	
+
 	function ngg_get_galleries() {
-		
+
 		if ( class_exists('nggdb') ) {
-      	
+
 	        // Settings of NextGEN Gallery SQL query
 	        $limit = 0;
 	        $start = 0;
 	        $order_by = 'title';
 	        $order_dir = 'ASC';
 	        $galleries = false;
-			
+
 	        // Seek to all NextGEN Galleries
 	        $gallerylist = nggdb::find_all_galleries( $order_by, $order_dir, true, $limit, $start, false);
 			return array( $gallerylist );
-			
+
 		}
-		
+
 	}
-	
-	
+
+
 	/*
 	*  ngg_get_albums()
 	*
@@ -204,27 +204,27 @@ class acf_field_ngg extends acf_field {
 	*  @param	n/a
 	*  @return	array of albums
 	*/
-	
+
 	function ngg_get_albums() {
-		
+
 		if ( class_exists('nggdb') ) {
-      	
+
 	        // Settings of NextGEN Albums SQL query
 	        $limit = 0;
 	        $start = 0;
 	        $order_by = 'name';
 	        $order_dir = 'ASC';
-			
+
 	        // Seek to all NextGEN Albums
 	        $albums = nggdb::find_all_album( $order_by, $order_dir, $limit, $start);
 			return array( $albums );
-			
+
 		}
-		
+
 	}
-	
-	
-	
+
+
+
 	/*
 	*  render_field()
 	*
@@ -239,32 +239,32 @@ class acf_field_ngg extends acf_field {
 	*  @param	$field (array) the $field being edited
 	*  @return	n/a
 	*/
-	
+
 	function render_field( $field ) {
-		
+
 		$values_gallery = array();
         $values_album = array();
-   
+
         $values = $field[ 'value' ];
-		
+
 		if ( !empty($values[0]) ) {
 	        foreach ( $values as $form ) {
 		        if ( in_array ( 'gallery', $form ) ) { $values_gallery[]=$form['ngg_id']; }
 		        if ( in_array ( 'album', $form ) ) { $values_album[]=$form['ngg_id']; }
 	        }
         }
-		
+
 		$galleries = $this->ngg_get_galleries();
 		$galleries = $galleries[0];
-		
+
 		$albumlist = $this->ngg_get_albums();
 		$albumlist = $albumlist[0];
-		
-	
+
+
 		$field['placeholder']	= __("Select",'acf');
-		
-		
-		
+
+
+
 		// atts
 		$atts = array(
 			//'name'				=> $field['name'],
@@ -276,120 +276,126 @@ class acf_field_ngg extends acf_field {
 			'data-placeholder'	=> $field['placeholder'],
 			'data-allow_null'	=> $field['allow_null']
 		);
-		
+
 		// ui
 		if( $field['ui'] ) {
-		
+
 			$atts['disabled'] = 'disabled';
 			$atts['class'] .= ' acf-hidden';
-			
+
 		}
-		
+
 		// multiple
 		if( $field['multiple'] == 'multiselect' ) {
-		
+
 			$atts['multiple']	= 'multiple';
 			$atts['size']		= $field[ 'multiple_size' ];
 			$atts['name']		.= '[]';
-			
+
 		} else {
-			
+
 			unset ($atts['multiple']);
-		
+
 		}
-		
+
 		// special atts
 		foreach( array( 'readonly', 'disabled' ) as $k ) {
-		
+
 			if( !empty($field[ $k ]) ) {
-			
+
 				$atts[ $k ] = $k;
 			}
-			
+
 		}
-		
+
 		// choices
-		
+
 		foreach( $galleries as $k => $v ) {
 			if( is_array($v) ){
-					
+
 					// optgroup
 					$els[] = array( 'type' => 'optgroup', 'label' => $v->title );
-					
+
 					if( !empty($v) ) {
-						
+
 						foreach( $v as $k2 => $v2 ) {
-							
+
 							$els[] = array( 'type' => 'option', 'value' => $k2, 'label' => $v2, 'selected' => in_array($k2, $field['value']) );
-							
+
 							$choices[] = $k2;
 						}
-						
+
 					}
-					
+
 					$els[] = array( 'type' => '/optgroup' );
-				
+
 				} else {
-					
-					$els[] = array( 'type' => 'option', 'value' => $v->gid, 'label' => $v->title, 'selected' => in_array($v->gid, $field['value']) );
-					
+
+					$els[] = array(
+						'type' => 'option',
+						'value' => $v->gid,
+						'label' => $v->title,
+						'selected' => $field['value'] ? in_array( $v->gid, $field['value'] ) : ''
+					);
+
 					$choices[$v->gid] = $v->title;
-					
+
 				}
 		}
-		
-		
+
 		// hidden input
 		if( $field['ui'] ) {
-			
+
 			// find real value based on $choices and $field['value']
 			//$real_value = array_intersect($field['value'], $choices);
-		
+
 			acf_hidden_input(array(
 				'type'	=> 'hidden',
 				'id'	=> $field['id'],
 				'name'	=> $field['name'],
 				'value'	=> implode(',', $choices)
 			));
-			
+
 		} elseif( $field['multiple'] ) {
-			
+
 			acf_hidden_input(array(
 				'type'	=> 'hidden',
 				'name'	=> $field['name'],
 			));
-			
+
 		}
 
 		// html
 		//print_r ($atts);
 		echo '<select name="' . $field['name'] . '[]" ' . acf_esc_attr( $atts ) . '>';
-		
+
 		?>
-		
+
+			<option value="0">No gallery</option>
+
             <?php if ( $field['nextgen_type'] == "Galleries and Albums" || $field['nextgen_type'] == "Galleries" ) : ?>
-              
+
             <optgroup label="<?php _e('Galleries','nggallery'); ?>">
             	<?php foreach( $galleries as $gallery ) : ?>
             	<option value="<?php echo $gallery->gid; ?>,gallery"<?php if ( $values_gallery ) selected( in_array( $gallery->gid, $values_gallery ) ); ?>><?php echo $gallery->title; ?></option>
             	<?php endforeach; ?>
             </optgroup>
 			<?php endif; ?>
-			
+
 			<?php if ( $field['nextgen_type'] == "Galleries and Albums" || $field['nextgen_type'] == "Albums" ) : ?>
 			<optgroup label="<?php _e('Albums','nggallery'); ?>">
-					<?php foreach( $albumlist as $album ) : ?>
+				<?php foreach( $albumlist as $album ) : ?>
 				<option value="<?php echo $album->id.',album'; ?>"<?php if ( $values_album ) selected( in_array( $album->id, $values_album ) ); ?>><?php echo $album->name; ?></option>
 				<?php endforeach; ?>
 			</optgroup>
 			<?php endif; ?>
-			
+
 		</select>
-		<?php 
+		<?php
 
 	}
-	
-		
+
+
 	/*
 	*  input_admin_enqueue_scripts()
 	*
@@ -404,27 +410,27 @@ class acf_field_ngg extends acf_field {
 	*  @return	n/a
 	*/
 
-	
-	
+
+
 	function admin_enqueue_scripts() {
-		
+
 		$dir = plugin_dir_url( __FILE__ );
-		
+
 		// register & include JS
 		wp_register_script( 'acf-ngg', "{$dir}js/input.js" );
 		wp_enqueue_script('acf-ngg');
-		
-		
+
+
 		// register & include CSS
-		//wp_register_style( 'acf-ngg', "{$dir}css/acf-ngg.css" ); 
+		//wp_register_style( 'acf-ngg', "{$dir}css/acf-ngg.css" );
 		//wp_enqueue_style('acf-ngg');
-		
-		
+
+
 	}
-	
-	
-	
-	
+
+
+
+
 	/*
 	*  input_admin_head()
 	*
@@ -440,21 +446,21 @@ class acf_field_ngg extends acf_field {
 	*/
 
 	/*
-		
+
 	function input_admin_head() {
-	
-		
-		
+
+
+
 	}
-	
+
 	*/
-	
-	
+
+
 	/*
    	*  input_form_data()
    	*
    	*  This function is called once on the 'input' page between the head and footer
-   	*  There are 2 situations where ACF did not load during the 'acf/input_admin_enqueue_scripts' and 
+   	*  There are 2 situations where ACF did not load during the 'acf/input_admin_enqueue_scripts' and
    	*  'acf/input_admin_head' actions because ACF did not know it was going to be used. These situations are
    	*  seen on comments / user edit forms on the front end. This function will always be called, and includes
    	*  $args that related to the current screen such as $args['post_id']
@@ -466,18 +472,18 @@ class acf_field_ngg extends acf_field {
    	*  @param	$args (array)
    	*  @return	n/a
    	*/
-   	
+
    	/*
-   	
+
    	function input_form_data( $args ) {
-	   	
-		
-	
+
+
+
    	}
-   	
+
    	*/
-	
-	
+
+
 	/*
 	*  input_admin_footer()
 	*
@@ -493,16 +499,16 @@ class acf_field_ngg extends acf_field {
 	*/
 
 	/*
-		
+
 	function input_admin_footer() {
-	
-		
-		
+
+
+
 	}
-	
+
 	*/
-	
-	
+
+
 	/*
 	*  field_group_admin_enqueue_scripts()
 	*
@@ -518,14 +524,14 @@ class acf_field_ngg extends acf_field {
 	*/
 
 	/*
-	
+
 	function field_group_admin_enqueue_scripts() {
-		
+
 	}
-	
+
 	*/
 
-	
+
 	/*
 	*  field_group_admin_head()
 	*
@@ -541,11 +547,11 @@ class acf_field_ngg extends acf_field {
 	*/
 
 	/*
-	
+
 	function field_group_admin_head() {
-	
+
 	}
-	
+
 	*/
 
 
@@ -563,18 +569,18 @@ class acf_field_ngg extends acf_field {
 	*  @param	$field (array) the field array holding all the field options
 	*  @return	$value
 	*/
-	
+
 	/*
-	
+
 	function load_value( $value, $post_id, $field ) {
-		
+
 		return $value;
-		
+
 	}
-	
+
 	*/
-	
-	
+
+
 	/*
 	*  update_value()
 	*
@@ -589,11 +595,11 @@ class acf_field_ngg extends acf_field {
 	*  @param	$field (array) the field array holding all the field options
 	*  @return	$value
 	*/
-	
-	
-	
+
+
+
 	function update_value( $value, $post_id, $field ) {
-		
+
 		$values = array();
 
 	      foreach( $value as $key=>$item ) {
@@ -605,14 +611,14 @@ class acf_field_ngg extends acf_field {
 	               $values[$key]['ngg_form'] = strval( $item );
 	            }
 	      }
-	
+
 	      return $values;
-		
+
 	}
-	
-	
-	
-	
+
+
+
+
 	/*
 	*  format_value()
 	*
@@ -628,22 +634,22 @@ class acf_field_ngg extends acf_field {
 	*
 	*  @return	$value (mixed) the modified value
 	*/
-		
-	
+
+
 	function format_value( $value, $post_id, $field ) {
-		
+
 		if ( $value[0]['ngg_form'] == 'null' ) {
 	         $value = false;
 		}
 
 		// return
 		return $value;
-		
+
 	}
-	
-	
-	
-	
+
+
+
+
 	/*
 	*  validate_value()
 	*
@@ -661,33 +667,33 @@ class acf_field_ngg extends acf_field {
 	*  @param	$input (string) the corresponding input name for $_POST value
 	*  @return	$valid
 	*/
-	
+
 	/*
-	
+
 	function validate_value( $valid, $value, $field, $input ){
-		
+
 		// Basic usage
 		if( $value < $field['custom_minimum_setting'] )
 		{
 			$valid = false;
 		}
-		
-		
+
+
 		// Advanced usage
 		if( $value < $field['custom_minimum_setting'] )
 		{
 			$valid = __('The value is too little!','acf-FIELD_NAME'),
 		}
-		
-		
+
+
 		// return
 		return $valid;
-		
+
 	}
-	
+
 	*/
-	
-	
+
+
 	/*
 	*  delete_value()
 	*
@@ -702,18 +708,18 @@ class acf_field_ngg extends acf_field {
 	*  @param	$key (string) the $meta_key which the value was deleted
 	*  @return	n/a
 	*/
-	
+
 	/*
-	
+
 	function delete_value( $post_id, $key ) {
-		
-		
-		
+
+
+
 	}
-	
+
 	*/
-	
-	
+
+
 	/*
 	*  load_field()
 	*
@@ -721,23 +727,23 @@ class acf_field_ngg extends acf_field {
 	*
 	*  @type	filter
 	*  @date	23/01/2013
-	*  @since	3.6.0	
+	*  @since	3.6.0
 	*
 	*  @param	$field (array) the field array holding all the field options
 	*  @return	$field
 	*/
-	
+
 	/*
-	
+
 	function load_field( $field ) {
-		
+
 		return $field;
-		
-	}	
-	
+
+	}
+
 	*/
-	
-	
+
+
 	/*
 	*  update_field()
 	*
@@ -750,18 +756,18 @@ class acf_field_ngg extends acf_field {
 	*  @param	$field (array) the field array holding all the field options
 	*  @return	$field
 	*/
-	
+
 	/*
-	
+
 	function update_field( $field ) {
-		
+
 		return $field;
-		
-	}	
-	
+
+	}
+
 	*/
-	
-	
+
+
 	/*
 	*  delete_field()
 	*
@@ -774,18 +780,18 @@ class acf_field_ngg extends acf_field {
 	*  @param	$field (array) the field array holding all the field options
 	*  @return	n/a
 	*/
-	
+
 	/*
-	
+
 	function delete_field( $field ) {
-		
-		
-		
-	}	
-	
+
+
+
+	}
+
 	*/
-	
-	
+
+
 }
 
 
